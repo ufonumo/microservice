43e37a7d4be826bd51bb8a69375a8fecb7b47e30
Ubuntu <ubuntu@ip-172-31-21-215.ec2.internal>
2022-08-16 15:41:10 +0000

    chore: contains docker file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath="ufonumo/microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy microservice --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/$(echo $(kubectl get pods) | awk -F ' ' '{print $6}') --address 0.0.0.0 8000:80

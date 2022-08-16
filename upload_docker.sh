2d13ef50ae38f31b8e473cca500c053c5c3897d7
Ubuntu <ubuntu@ip-172-31-21-215.ec2.internal>
2022-08-16 15:31:01 +0000

    chore: contains docker file

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath="ufonumo/microservice"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login && docker image tag microservice $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
